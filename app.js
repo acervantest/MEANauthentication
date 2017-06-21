5e10bd4e1e70e67c0401afe15c32eba2f908b8bb
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-06-20 23:04:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Port Number
+const port = 3000;
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+//Start Server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
