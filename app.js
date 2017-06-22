a062f5b27a32ab4afd6f1a663ffe61cb9c8f3d4b
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-06-21 21:14:03 -0700

    database connection and routes added

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to DB
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: ' + config.database);
+});
+
+//On Error Connection
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+
 //Port Number
 const port = 3000;
+
+//CORS MiddleWare
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser MiddleWare
+app.use(bodyParser.json());
+
+app.use('/users', users);
 
 //Index Route
 app.get('/', (req, res) => {
